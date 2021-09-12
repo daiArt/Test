cdb4b1dfe63d8957f2d72eb2158d98622ec9c59b
dai-design <83054427+daiArt@users.noreply.github.com>
2021-09-12 21:34:04 +0900

    a

---
+++ secondPage/addCode/second.js
@@ -0,0 +1,9 @@
+$(function() {
+
+    $(".okBox").click(function() {
+        const testTarget = document.getElementById("hello")
+        let test = testTarget.value;
+        console.log(test)
+    })
+
+}) 
