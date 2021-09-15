26b1278d71044d60bd8ebfb81e5d0345e65c1a93
dai-design <83054427+daiArt@users.noreply.github.com>
2021-09-15 10:10:45 +0900

    a

--- secondPage/addCode/second.js
+++ secondPage/addCode/second.js
@@ -227,22 +227,22 @@ $(function() {
     })
 
 
-    $(".profileCheck > .okBox").click(function() {
-        alert('ok')
+    // $(".profileCheck > .okBox").click(function() {
+    //     alert('ok')
 
-        let testSum = localStorage.getItem('playerSum');
-        let targetName = [];
+    //     let testSum = localStorage.getItem('playerSum');
+    //     let targetName = [];
 
-        for (i = 0; 1 <= 4; i++) {
+    //     for (i = 0; 1 <= 4; i++) {
 
-            let strage = localStorage.getItem('player' + i);
+    //         let strage = localStorage.getItem('player' + i);
 
-            targetName[i] = 'プレイヤー' + i +strage
-        }
+    //         targetName[i] = 'プレイヤー' + i +strage
+    //     }
 
-        alert('プレイ人数は' + testSum);
-        alert('プレイヤーは' + testName)
+    //     alert('プレイ人数は' + testSum);
+    //     alert('プレイヤーは' + testName)
 
-    })
+    // })
 
 }) 
